2124c3f8e53cb19c10cef5f6a315b313640aec91
i0s_tweak3r <djvs23@gmail.com>
2026-03-16 17:18:42 -0400

    Refactor application launch method for clarity
    added notificatiobn when returning from background for sora polling

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,14 +1,40 @@
+// AppDelegate.m
+// EZCompleteUI
+ 
 #import "AppDelegate.h"
 #import "ViewController.h"
-
+ 
 @implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+ 
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+ 
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    ViewController *mainViewController = [[ViewController alloc] init];
-    self.window.rootViewController = mainViewController;
+    self.window.rootViewController = [[ViewController alloc] init];
     [self.window makeKeyAndVisible];
     return YES;
 }
-
+ 
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Short delay so ViewController.viewDidLoad is guaranteed to have run
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)),
+                   dispatch_get_main_queue(), ^{
+        [[NSNotificationCenter defaultCenter]
+            postNotificationName:@"EZAppDidBecomeActive" object:nil];
+    });
+}
+ 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    __block UIBackgroundTaskIdentifier bgTask = UIBackgroundTaskInvalid;
+    bgTask = [application beginBackgroundTaskWithExpirationHandler:^{
+        [application endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    }];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
+                   dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [application endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    });
+}
+ 
 @end
